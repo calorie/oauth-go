263dad8cfb77b0908dddad7e3a9d75fde111e107
Yu Shigetani <s2g4t1n2@gmail.com>
2023-08-05 17:04:34 +0900

    swagger

--- main.go
+++ main.go
@@ -4,15 +4,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/calorie/oauth-go/docs"
+	"github.com/swaggo/files"
+	"github.com/swaggo/gin-swagger"
 )
 
+// @BasePath /v1
+
+// Health godoc
+// @Summary     get health
+// @Description get health
+// @Tags        health
+// @Accept      json
+// @Produce     json
+// @Success     204 {object} domain.HTTPEmpty
+// @Router      /health [get]
 func health(c *gin.Context) {
 	c.Status(http.StatusNoContent)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/health", health)
+
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/health", health)
+	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run(":8080")
 }
